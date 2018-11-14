62ab0de85e49a8b0dff5c23c1c4c853fb48bd625
Curso fisi4028  je.forero 2018-09-26 <fisi4028@clustergate.local>
2018-11-13 21:28:35 -0500

    preclass finished

---
+++ listing12_3/m.neira10_1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#PBS -l nodes=1:ppn=4,mem=1gb,walltime=00:10:00
+#PBS -M m.neira10@uniandes.edu.co
+#PBS -m abe
+#PBS -N mneira10_listing12_3
+#PBS -j oe
+
+
+cd /hpcfs/home/fisi4028/m.neira10/MauricioNeira_Ejercicio27/listing12_3
+make
